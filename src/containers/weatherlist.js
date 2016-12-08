b45cf57a4c622674edd867c6404611c38334bc25
Nicklas Winger <nwi@gecko.no>
2016-12-08 13:08:03 +0100

    Improve chart component and weatherlist

--- src/containers/weatherlist.js
+++ src/containers/weatherlist.js
@@ -8,16 +8,16 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = _.map(cityData.list, (weather) => {
-            return weather.main.temp;
-        });
+        const temps = _.map(cityData.list, (weather) => { return weather.main.temp; });
+        const pressures = _.map(cityData.list, (weather) => { return weather.main.pressure; });
+        const humidities = _.map(cityData.list, (weather) => { return weather.main.humidity; });
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Chart data={temps} color="orange" />
-                </td>
+                <td><Chart data={temps} color="orange" units="K"/></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="black" units="%"/></td>
             </tr>
         );
     }
@@ -28,15 +28,13 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        _.map(this.props.weather, this.renderWeather)
-                    }
+                    {_.map(this.props.weather, this.renderWeather)}
                 </tbody>
             </table>
         );
